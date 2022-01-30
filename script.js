d5424b40ba5db559406ceadc225813cd1a8bee36
Abhay Kumar <kumarabhay401@gmail.com>
2022-01-30 15:50:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,23 +9,15 @@ async function getCatPics() {
     showpic.innerHTML = '';
 
     try {
-         await fetch("https://api.thecatapi.com/v1/images/search", {
-            method: "GET",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
-            .then((response) => response.json()).then((data) => {
-                let imgUrl = data[0].url;//take only only url from the api object
-                // console.log(imgUrl);
-                let createImgElement = document.createElement('img');//Creat a img element
-                createImgElement.setAttribute('src', `${imgUrl}`);
-                let showpic = document.querySelector('.ImgDiv')
-                showpic.appendChild(createImgElement);
-            })
-    }
-    catch (error) {
+        let data = await fetch("https://api.thecatapi.com/v1/images/search")
+        let response = await data.json()
+        // console.log(response[0].url)
+
+        showpic.innerHTML = `<img src="${response[0].url}" width="300" height="280" alt="image"/>`
+
+
+    } catch (error) {
         console.log(error);
     }
 }
-getCatPics();
+
